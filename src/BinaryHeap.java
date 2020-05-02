36060688902916368290c30128b457be82d185ec
Lewis Bonathan Metcalf <lewis.metcalf@thoughtworks.com>
2020-05-03 00:44:45 +0200

    impl binaryHeap insertion

--- src/BinaryHeap.java
+++ src/BinaryHeap.java
@@ -1,18 +1,46 @@
 public class BinaryHeap {
     int N;
     int[] a;
+        //binary heap shape as array 0 - 1/2 - 3,4/5,6 - 7,8/9,10/11,12/13,14
 
-    public BinaryHeap() { }
+    public BinaryHeap(int cap) {
+        a = new int[cap];
+        N = 0;
+    }
 
     public int max() {
-        return 0;
+        return a[0];
     }
 
     public int deleteMax() {
         return 0;
     }
 
-    public void insert(int i) {
+    public void insert(int val) {
+        a[N] = val; //insert at bottom
+        swim(N);
+        N++;
+    }
 
+    private void swim(int i) {
+        int parentIndex = parent(i);
+
+        // while bigger than parent, swap
+        while (parentIndex >= 0 && a[i] > a[parentIndex]) {
+            swap(a, i, parentIndex);
+            i = parentIndex;
+            parentIndex = parent(parentIndex);
+        }
+
+    }
+
+    private void swap(int[] a, int i, int i2) {
+        int temp = a[i];
+        a[i] = a[i2];
+        a[i2] = temp;
+    }
+
+    private int parent(int i) {
+        return Math.round((i + 0.5f) / 2) - 1;
     }
 }
--- test/BinaryHeapTest.java
+++ test/BinaryHeapTest.java
@@ -1,17 +1,28 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BinaryHeapTest {
 
-    @Test
-    void shouldActAsAHeap() {
-        BinaryHeap heap = new BinaryHeap();
+    private BinaryHeap heap;
+
+    @BeforeEach
+    void setUp() {
+        heap = new BinaryHeap(4);
         heap.insert(5);
         heap.insert(2);
         heap.insert(9);
         heap.insert(7);
+    }
 
+    @Test
+    void shouldHaveMaxAtTop() {
+        assertEquals(9, heap.max());
+    }
+
+    @Test
+    void shouldActAsAHeap() {
         assertEquals(9, heap.deleteMax());
         assertEquals(7, heap.deleteMax());
         assertEquals(5, heap.deleteMax());
